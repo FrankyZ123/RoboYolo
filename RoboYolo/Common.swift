01f2232f1ab3cc7ea2157ee6d6eef89ec9d4e9b7
FrankyZ123 <frankzanesand@gmail.com>
2021-03-20 12:33:55 -0500

    Indexing Change to Common.swift

--- RoboYolo/Common.swift
+++ RoboYolo/Common.swift
@@ -14,13 +14,13 @@ func returnIter(topNSelector: String, resultsLength: Int) -> Int {
     var iter = 0
     
     if topNSelector == "Top-1" { // Top-1
-        iter = 1
+        iter = 0 // swift is 0 indexed, this will ensure only the top classification is checked
     } else if topNSelector == "Top-5" { // Top-5
-        iter = 5
+        iter = 4 // go through the first 5 (0, 1, 2, 3, 4) indeces
     } else if topNSelector == "Top-10" { // Top-10
-        iter = 10
+        iter = 9 // ...
     } else { // Top-N
-        iter = resultsLength - 1
+        iter = resultsLength - 1 // go through all the results
     }
     
     return iter
